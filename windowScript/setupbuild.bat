243ccfc4593eed6fd45d34b4668fcdfeeb7e772b
tmw <694497013@qq.com>
2023-08-23 17:46:52 +0800

    1

---
+++ windowScript/setupbuild.bat
@@ -0,0 +1,23 @@
+xcopy C:\Users\Administrator\AppData\Local\Google U:\Users\Administrator\AppData\Local\Google /E/H/C/I
+mklink /J C:\Users\Administrator\AppData\Local\Google U:\Users\Administrator\AppData\Local\Google
+
+xcopy C:\Program Files\Google U:\Program Files\Google /E/H/C/I
+mklink /J C:\Program Files\Google U:\Program Files\Google
+
+::=================================================================
+xcopy C:\Users\Administrator\.cargo u:\Users\Administrator\.cargo /E/H/C/I
+mklink /J C:\Users\Administrator\.cargo u:\Users\Administrator\.cargo
+
+xcopy C:\Users\Administrator\.rustup u:\Users\Administrator\.rustup /E/H/C/I
+mklink /J C:\Users\Administrator\.rustup u:\Users\Administrator\.rustup
+
+xcopy C:\Users\Administrator\.gradle u:\Users\Administrator\.gradle /E/H/C/I
+mklink /J C:\Users\Administrator\.gradle u:\Users\Administrator\.gradle
+
+xcopy C:\Users\Administrator\.android u:\Users\Administrator\.android /E/H/C/I
+mklink /J C:\Users\Administrator\.android u:\Users\Administrator\.android
+
+
+::=================================================================
+xcopy C:\Users\Administrator\AppData\Local\JetBrains U:\Users\Administrator\AppData\Local\JetBrains /E/H/C/I
+mklink /J C:\Users\Administrator\AppData\Local\JetBrains U:\Users\Administrator\AppData\Local\JetBrains
