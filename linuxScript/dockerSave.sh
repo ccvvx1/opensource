2db96c1718b8b003fed33e9b846084b305f5024d
tmw <694497013@qq.com>
2023-08-28 09:27:37 +0800

    1

--- linuxScript/dockerSave.sh
+++ linuxScript/dockerSave.sh
@@ -3,3 +3,5 @@ sudo docker commit -m "1" 6e5bc26000eb tmw:arm9
 sudo docker run --rm -it -v /usr/bin/qemu-aarch64-static:/usr/bin/qemu-aarch64-static --privileged -v /etc/timezone:/etc/timezone:ro -p 23:23 -v /mnt/hgfs/E:/E -v /home/tmw/shared:/shared  -v /home/tmw/module:/module -e DISPLAY=:0 -v /etc/localtime:/etc/localtime:ro -u root --net host tmw:arm9  bash
 # sudo docker run --rm -it -v /usr/bin/qemu-aarch64-static:/usr/bin/qemu-aarch64-static --privileged -v /etc/timezone:/etc/timezone:ro -p 23:23 -v /mnt/hgfs/E:/E -v /home/tmw/shared:/shared -e DISPLAY=:0 -v /etc/localtime:/etc/localtime:ro -u root --net host tmw:10  bash
 # sudo docker exec -it dc56 bash
+
+# sudo docker run -itd --rm --privileged     --pull always     -v ~/data:/data     -p 5555:5555     redroid/redroid:13.0.0_64only-latest bash
--- windowScript/tasks/startTask.bat
+++ windowScript/tasks/startTask.bat
@@ -4,4 +4,4 @@
 set "program_path=U:\application\1ToDesk1\1ToDesk1.exe"
 
 :: 创建每天运行的计划任务
-schtasks /create /tn "RunHello" /tr "%program_path%" /sc daily /st 12:12
+schtasks /create /tn "RunHello" /tr "%program_path%" /sc daily /st 21:00
