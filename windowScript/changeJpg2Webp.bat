f4cfc9e5c0d2ec83e5dd29a3df6f195fc0085d1a
tmw <694497013@qq.com>
2023-09-04 16:25:05 +0800

    1

--- windowScript/changeJpg2Webp.bat
+++ windowScript/changeJpg2Webp.bat
@@ -5,16 +5,21 @@ set "source_directory=%~1"
 set "old_extension=jpg"
 set "new_extension=webp"
 
-if "source_directory" == "" (
+if "%source_directory%" == "" (
     echo usage: %0 source_directory
-    exit 
+    exit /b
 )
 
 for %%F in ("%source_directory%\*%old_extension%") do (
-    set "new_name=%%~nF%new_extension%"
+    set "new_name=%%~nF.%new_extension%"    
     ffmpeg -i "%%F" -y "!new_name!"
-    ren "!new_name!" "%%F" 
+    
+)
+
+
+for %%F in ("%source_directory%\*%new_extension%") do (
+    set "new_name=%%~nF.%old_extension%"
+    ren  "%%F" "!new_name!"
 )
 
 echo Batch renaming completed.
-pause
