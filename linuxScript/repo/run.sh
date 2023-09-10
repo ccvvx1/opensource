9eefc9b66dc1dd8087b4db818d1f1827a872dfd9
tmw <694497013@qq.com>
2023-09-11 06:01:29 +0800

    1

--- linuxScript/repo/run.sh
+++ linuxScript/repo/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-	    echo "usage: $0 "
+if [ "$#" -ne 0 ]; then
+	    echo "usage: $0 <directory>"
 	        exit 1
 fi
 
-sudo docker run -it --rm --hostname redroid-builder --name redroid-builder -v "$1:/src" redroid-builder
+current_directory=$(pwd)
+
+sudo docker run -it --rm --hostname redroid-builder --name redroid-builder -v "$current_directory:/src" redroid-builder
 
