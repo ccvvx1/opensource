b3d94d428d790c22c73584915f9f1ed63d8747f0
tmw <694497013@qq.com>
2023-09-10 12:07:15 +0800

    1

--- linuxScript/cmdList.sh
+++ linuxScript/cmdList.sh
@@ -3,4 +3,5 @@ echo    ml             --make link
 echo    sm             --mount disk
 echo    rp             --newest repo
 echo    rpr            --run repo docker
+echo    cl             --cmd list
 echo    ========================
