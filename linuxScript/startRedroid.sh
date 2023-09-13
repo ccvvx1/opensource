b6fcb04f9122ca14392a2b76e91cf9618ec39315
tmw <694497013@qq.com>
2023-09-13 14:55:24 +0800

    1

--- linuxScript/startRedroid.sh
+++ linuxScript/startRedroid.sh
@@ -1,1 +1,1 @@
-sudo docker run -itd --rm --privileged  --pull always  -v ~/data:/data  -p 5555:5555  redroid/redroid:11.0.0-latest
+sudo docker run -itd --rm --privileged  --pull always -p 30001:30001 -p 9:9  -p 5555:5555    -p 8888:8888 redroid/redroid:11.0.0-latest
