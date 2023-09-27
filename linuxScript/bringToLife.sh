d0658a43246ca08a01701b498e30f2c053db3ee3
tmw <694497013@qq.com>
2023-09-27 18:03:23 +0800

    1

--- linuxScript/bringToLife.sh
+++ linuxScript/bringToLife.sh
@@ -1,3 +1,23 @@
-cd /mnt/tmw/module/python/bring/photo_restoration/
-. /mnt/tmw/module/python/bring/venv/bin/activate1
-python run.py --input_folder ./test_images/old --output_folder ./output/ --GPU 0
+#!/bin/bash
+
+# 检查参数数量
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 [0|1]"
+    exit 1
+fi
+
+# 参数为0时执行操作
+if [ $1 -eq 0 ]; then
+    echo "recover"
+    cd /mnt/tmw/module/python/bring/photo_restoration/
+    . /mnt/tmw/module/python/bring/venv/bin/activate1
+    python run.py --input_folder ./test_images/old --output_folder ./output/ --GPU 0
+
+fi
+
+# 参数为1时执行操作
+if [ $1 -eq 1 ]; then
+    echo "amend"
+    rm -rf ./output/*
+    python run.py --input_folder ./test_images/old_w_scratch/ --output_folder ./output/ --GPU 0 --with_scratch
+fi
