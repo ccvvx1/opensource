ec743945efaeaf42b4aa75b8fef83d7936004eed
tmw <694497013@qq.com>
2023-09-27 15:33:09 +0800

    1

--- linuxScript/sadtalk.sh
+++ linuxScript/sadtalk.sh
@@ -1,2 +1,3 @@
+. venv/bin/activate1
 export CUDA_VISIBLE_DEVICES=-1
 python3.8 inference.py --driven_audio ./examples/driven_audio/RD_Radio31_000.wav  --source_image examples/source_image/full3.png  --result_dir ./results --still --preprocess full --enhancer gfpgan
--- linuxScript/soVitsSvc.sh
+++ linuxScript/soVitsSvc.sh
@@ -1,1 +1,2 @@
+. venv/bin/activate1
 python inference_main.py -m "logs/44k/G_0.pth" -c "configs/config.json" -n "ok1.wav" -t 0 -s "tsukuyomi"
