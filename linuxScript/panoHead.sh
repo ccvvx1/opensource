45fcd70d84c10366d77ea6c97df8469310ff5e84
tmw <694497013@qq.com>
2023-09-28 21:05:17 +0800

    1

---
+++ linuxScript/panoHead.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 [v|i]"
+  exit 1
+fi
+
+
+source /mnt/tmw/module/python/PanoHead/venv/bin/activate1
+
+
+export PYTHONPATH=/mnt/tmw/module/python/PanoHead/PanoHead:$PYTHONPATH
+
+
+cd /mnt/tmw/module/python/PanoHead/PanoHead
+
+if [ $1 -eq v ]; then
+    python gen_videos.py --network /content/PanoHead/models/easy-khair-180-gpc0.8-trans10-025000.pkl --seeds 0-3 --grid 1x1 --output=test.mp4 --cfg Head --trunc 0.7
+
+fi
+
+if [ $1 -eq i ]; then
+    python gen_samples.py --outdir=out --shape-format=.ply --trunc=0.7 --shapes=true --seeds=0-3 --network /content/PanoHead/models/easy-khair-180-gpc0.8-trans10-025000.pkl
+fi
