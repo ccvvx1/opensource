20ef20652825c2b25ed70d2595141eaea0a8bab1
tmw <694497013@qq.com>
2023-09-28 21:23:21 +0800

    1

--- linuxScript/panoHead.sh
+++ linuxScript/panoHead.sh
@@ -16,10 +16,10 @@ export PYTHONPATH=/mnt/tmw/module/python/PanoHead/PanoHead:$PYTHONPATH
 cd /mnt/tmw/module/python/PanoHead/PanoHead
 
 if [ $1  == "v"  ]; then
-    python gen_videos.py --network /content/PanoHead/models/easy-khair-180-gpc0.8-trans10-025000.pkl --seeds 0-3 --grid 1x1 --output=test.mp4 --cfg Head --trunc 0.7
+    python gen_videos.py --network ./models/easy-khair-180-gpc0.8-trans10-025000.pkl --seeds 0-3 --grid 1x1 --output=test.mp4 --cfg Head --trunc 0.7
 
 fi
 
 if [ $1  == "i"  ]; then
-    python gen_samples.py --outdir=out --shape-format=.ply --trunc=0.7 --shapes=true --seeds=0-3 --network /content/PanoHead/models/easy-khair-180-gpc0.8-trans10-025000.pkl
+    python gen_samples.py --outdir=out --shape-format=.ply --trunc=0.7 --shapes=true --seeds=0-3 --network ./models/easy-khair-180-gpc0.8-trans10-025000.pkl
 fi
