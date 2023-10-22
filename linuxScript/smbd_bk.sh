429d226be2f0da340e06cc92f4afa058206aba76
tmw <694497013@qq.com>
2023-10-22 08:11:52 +0800

    1

---
+++ linuxScript/smbd_bk.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt install samba
+mkdir /home/tmw/shared
+sudo chmod 777 -R /home/tmw/shared
+sudo chmod 777 /etc/samba/smb.conf
+sudo echo "[shared]" >> /etc/samba/smb.conf
+sudo echo "path = /home/tmw/shared" >> /etc/samba/smb.conf
+sudo echo "read only = no" >> /etc/samba/smb.conf
+sudo echo "guest ok = yes" >> /etc/samba/smb.conf
+sudo service smbd restart
+sudo apt install net-tools
+sudo chmod 777 /home/tmw
+ifconfig
+
