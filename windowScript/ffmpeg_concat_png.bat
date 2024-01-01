c40b63254da2a01f640f2cabef73f8e914f4e4f2
tmw <694497013@qq.com>
2024-01-01 22:55:45 +0800

    1

--- windowScript/ffmpeg_concat_png.bat
+++ windowScript/ffmpeg_concat_png.bat
@@ -2,7 +2,7 @@
 
 REM 检查输入参数数量
 if "%~3"=="" (
-    echo 用法: %~nx0 输入图片1 输入图片2 输出图片
+    echo 用法: %~nx0 输入图片1 输入图片2 输出图片 (ps:不要加png结尾，需要创建output文件夹)
     exit /b 1
 )
 
@@ -11,9 +11,9 @@ set image2=%2
 set output=%3
 
 echo 正在使用以下命令合并图片：
-echo ffmpeg -i %image1%.png -i %image2%.png -filter_complex hstack=inputs=2 output\%output%.png
+echo ffmpeg -i %image1%.png -i %image2%.png -filter_complex hstack=inputs=2 -y output\%output%.png
 
 REM 执行ffmpeg命令
-ffmpeg -i %image1%.png -i %image2%.png -filter_complex hstack=inputs=2 output\%output%.png
+ffmpeg -i %image1%.png -i %image2%.png -filter_complex hstack=inputs=2 -y output\%output%.png
 
 echo 合并完成。输出图片保存为: %output%
