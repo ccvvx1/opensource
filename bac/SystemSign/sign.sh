88f6f8a632a7c65e00090d0c00cd8cb08c2622d2
tanmingwu <13802885497@139.com>
2024-01-05 10:55:52 +0800

    1

---
+++ bac/SystemSign/sign.sh
@@ -0,0 +1,23 @@
+@echo off
+
+if "%1"=="" (
+    echo Please provide source file path.
+    goto :eof
+)
+
+set src=%1
+
+if not exist "%src%" (
+    echo Source file not found: %src%
+    goto :eof
+)
+
+set des=%2
+
+if not defined des (
+    set des=sign_%~n1
+)
+
+java -jar signapk.jar U:\module\python\opensource\bac\SystemSign\platform.x509.pem U:\module\python\opensource\bac\SystemSign\platform.pk8 %src% ..\%des%
+
+echo Signed file: %des%, System sign done...
